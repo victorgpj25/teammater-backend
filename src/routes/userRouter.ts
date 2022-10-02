184727d924335ed66d2c82ac1f8bc76344c6d581
Victor <victorgpj25@gmail.com>
2022-10-01 22:11:32 -0300

    feat: sign-in route

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 
-import { signUp } from '../controllers/userController'
-import { validateSignUpReqBody } from '../middlewares/userMiddleware'
+import { signUp, signIn } from '../controllers/userController'
+import { validateSignUpReqBody, validateSignInReqBody } from '../middlewares/userMiddleware'
 
 const userRouter = express.Router()
 
 userRouter.post("/signup", validateSignUpReqBody, signUp)
+userRouter.post('/signin', validateSignInReqBody, signIn)
 
 export default userRouter
