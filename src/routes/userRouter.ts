30d53ea233fe74212844f9ce56ff02d7a463c7bb
Victor <victorgpj25@gmail.com>
2022-10-02 17:51:32 -0300

    feat: get player route

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,11 +1,14 @@
-import express from 'express'
+import express from "express"
 
-import { signUp, signIn } from '../controllers/userController'
-import { validateSignUpReqBody, validateSignInReqBody } from '../middlewares/userMiddleware'
+import { signUp, signIn, getRandomPlayer } from "../controllers/userController"
+import { validateSignUpReqBody, validateSignInReqBody } from "../middlewares/userMiddleware"
+import { verifyToken } from "../middlewares/authMiddleware"
 
 const userRouter = express.Router()
 
 userRouter.post("/signup", validateSignUpReqBody, signUp)
-userRouter.post('/signin', validateSignInReqBody, signIn)
+userRouter.post("/signin", validateSignInReqBody, signIn)
+
+userRouter.get("/player", verifyToken, getRandomPlayer)
 
 export default userRouter
