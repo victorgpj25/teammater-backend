53b7177c0921814151ddb25186cf87b3c9f3384c
Victor <victorgpj25@gmail.com>
2022-10-02 20:09:14 -0300

    feat: ask for/skip teammate route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 
 import userRouter from './userRouter'
+import relationsRouter from './relationsRouter'
 
 const router = express.Router()
 
 router.use(userRouter)
+router.use(relationsRouter)
 
 export default router
---
+++ src/routes/relationsRouter.ts
@@ -0,0 +1,14 @@
+import express from "express"
+
+import { askForTeammate, skipTeammate } from "../controllers/userController"
+import { verifyReqParams } from "../middlewares/reqParamsMiddleware"
+import { verifyToken } from "../middlewares/authMiddleware"
+
+const relationsRouter = express.Router()
+
+relationsRouter.post("/player/ask/:id", verifyToken, verifyReqParams, askForTeammate)
+relationsRouter.post("/player/skip/:id", verifyToken, verifyReqParams, skipTeammate)
+
+
+
+export default relationsRouter
